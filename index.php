2041fb19854e9952a545510ba420daa75779ff9f
Anand Barai <ab20892@gmail.com>
2023-10-09 14:15:07 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -10,14 +10,9 @@ include('connect.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Task</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <style>
         input[type="number"]::-webkit-outer-spin-button,
@@ -35,8 +30,7 @@ include('connect.php');
 
 <body>
     <div class="container-fluid mt-3">
-        <button type="submit" name="view" class="bg-primary text-white border-0 p-1"><a
-                href="addProduct.php">&nbsp;&nbsp;&nbsp;Add Product&nbsp;&nbsp;&nbsp;</a></button>
+        <button type="submit" name="view" class="bg-primary text-white border-0 p-1"><a href="addProduct.php">&nbsp;&nbsp;&nbsp;Add Product&nbsp;&nbsp;&nbsp;</a></button>
     </div>
     <h2 class="text-center">Add Customer & Product Details Form</h2>
     <!-- Add Data Form -->
@@ -44,8 +38,7 @@ include('connect.php');
         <div class="container">
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_title" class="form-label">Customer Name :</label>
-                <input type="text" name="vCustomerName" id="vCustomerName" class="form-control"
-                    placeholder="Enter Customer Name" autocomplete="off" required>
+                <input type="text" name="vCustomerName" id="vCustomerName" class="form-control" placeholder="Enter Customer Name" autocomplete="off" required>
             </div>
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_title" class="form-label">Product Name :</label>
@@ -58,7 +51,7 @@ include('connect.php');
 
                     $row = mysqli_query($myCon, $sql);
                     // Create an array to store the product names
-                    
+
                     while ($result = mysqli_fetch_assoc($row)) {
                         $iProductId = $result['iProductId'];
                         $vProductName = $result['vProductName'];
@@ -78,27 +71,22 @@ include('connect.php');
             </div>
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_title" class="form-label">Discount (%) :</label>
-                <input type="number" name="iDiscount" id="iDiscount" class="form-control" placeholder="Enter Discount"
-                    autocomplete="off" required>
+                <input type="number" name="iDiscount" id="iDiscount" class="form-control" placeholder="Enter Discount" autocomplete="off" required>
             </div>
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_title" class="form-label">Quantity :</label>
-                <input type="number" name="iQty" id="iQty" class="form-control" placeholder="Enter Quantity"
-                    autocomplete="off" required>
+                <input type="number" name="iQty" id="iQty" class="form-control" placeholder="Enter Quantity" autocomplete="off" required>
             </div>
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_title" class="form-label">Net Amount :</label>
-                <input type="text" name="iNetAmount" id="iNetAmount" placeholder="Rate – Disc Of Rate"
-                    class="form-control" autocomplete="off" readonly>
+                <input type="text" name="iNetAmount" id="iNetAmount" placeholder="Price – Disc Of Price" class="form-control" autocomplete="off" readonly>
             </div>
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_title" class="form-label">Total Amount :</label>
-                <input type="text" name="iTotalAmount" id="iTotalAmount" class="form-control"
-                    placeholder="Net Amount * Qty" autocomplete="off" readonly>
+                <input type="text" name="iTotalAmount" id="iTotalAmount" class="form-control" placeholder="Net Amount * Quantity" autocomplete="off" readonly>
             </div>
             <div class="form-outline mb-4 w-50 m-auto">
-                <input type="button" name="AddData" id="addData" class="bg-primary text-white border-0 p-1"
-                    value="    Add    ">
+                <input type="button" name="AddData" id="addData" class="bg-primary text-white border-0 p-1" value="    Add    ">
             </div>
         </div>
     </form>
@@ -123,18 +111,17 @@ include('connect.php');
     </div>
 
     <div class="form-outline mb-4 w-50 m-auto">
-        <input type="button" name="insertData" id="insertData" class="bg-primary text-white border-0 p-1"
-            value="    Submit    ">
+        <input type="button" name="insertData" id="insertData" class="bg-primary text-white border-0 p-1" value="    Submit    ">
     </div>
 </body>
 
 </html>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
 
         //function to fetch and store value of unit and price 
-        $('#vProductName').change(function () {
+        $('#vProductName').change(function() {
             var iProductId = $(this).val();
             // console.log(iProductId);
             $.ajax({
@@ -143,7 +130,7 @@ include('connect.php');
                     'action': 'getRateUnits',
                     iProductId: iProductId
                 },
-                success: function (data) {
+                success: function(data) {
                     // Decode the JSON data
                     var jsonData = JSON.parse(data);
 
@@ -162,32 +149,32 @@ include('connect.php');
         });
 
         //function to calculate Net Amount
-        $('#iDiscount').change(function () {
+        $('#iDiscount, #iQty').change(function() {
 
             // Calculate the net amount
             const iPrice = $('#iPrice').val();
-            const iDiscount = $(this).val();
-            const iNetAmount = iPrice - (iDiscount / 100) * iPrice;
+            const iDiscount = $('#iDiscount').val();
+            var iNetAmount = iPrice - (iDiscount / 100) * iPrice;
 
             // Set the value of the net amount input field
             $('#iNetAmount').val(iNetAmount);
-        });
-
-        //function to calculate Total Amount
-        $('#iQty').change(function () {
 
             // Calculate the net amount
-            const iNetAmount = $('#iNetAmount').val();
-            const iQty = $(this).val();
+            var iNetAmount = $('#iNetAmount').val();
+            const iQty = $('#iQty').val();
             const iTotal = iNetAmount * iQty;
-            // console.log(iQty);
+            console.log(iQty);
+            console.log(iDiscount);
             // console.log(iTotal);
             // Set the value of the net amount input field
             $('#iTotalAmount').val(iTotal);
         });
 
+        //function to calculate Total Amount
+      
 
-        $('#addData').click(function () {
+
+        $('#addData').click(function() {
             var selectedProductName = $('#vProductName option:selected').text();
             // Get the form data
             var formData = {
@@ -201,32 +188,51 @@ include('connect.php');
                 iTotalAmount: $('#iTotalAmount').val()
             };
 
-            const newRow = $('<tr></tr>');
             // console.log(selectedProductName);
-            // Add the form data to the new row
-            newRow.append(`<td><input type="text" class="form-control" id="ProductName" value="${formData.vProductName}" readonly/></td>`);
-            newRow.append(`<td><input type="number" class="form-control" id="ProductPrice" value="${formData.iPrice}" readonly/></td>`);
-            newRow.append(`<td><input type="number" class="form-control" id="ProductUnit" value="${formData.iUnit}" readonly/></td>`);
-            newRow.append(`<td><input type="number" class="form-control" id="iDiscountedPrice" value="${formData.iDiscount}" readonly/></td>`);
-            newRow.append(`<td><input type="number" class="form-control" id="ProductQty" value="${formData.iQty}" readonly/></td>`);
-            newRow.append(`<td><input type="number" class="form-control" id="ProductNetAmount" value="${formData.iNetAmount}" readonly/></td>`);
-            newRow.append(`<td><input type="number" class="form-control" id="ProductTotalAmount" value="${formData.iTotalAmount}" readonly/></td>`);
-            newRow.append(`<td><button class="bg-danger text-white border-0 p-1" id="removeAllButton">&nbsp;&nbsp;Remove&nbsp;&nbsp;</button></td>`);
+            // Create a new row in the table
+            var currentID = 1;
+            currentID++;
+
+            // Create a new row in the table
+            const newRow = $('<tr></tr>');
+
+            // Add the form data and ID value to the new row
+            newRow.append(`<td><input type="text" class="form-control" id="${formData.vProductName}_${currentID}" value="${formData.vProductName}" readonly/></td>`);
+            newRow.append(`<td><input type="number" class="form-control" id="${formData.iPrice}_${currentID}" value="${formData.iPrice}" readonly/></td>`);
+            newRow.append(`<td><input type="number" class="form-control" id="${formData.iUnit}_${currentID}" value="${formData.iUnit}" readonly/></td>`);
+            newRow.append(`<td><input type="number" class="form-control" id="${formData.iDiscount}_${currentID}" value="${formData.iDiscount}" /></td>`);
+            newRow.append(`<td><input type="number" class="form-control" id="${formData.iQty}_${currentID}" value="${formData.iQty}" /></td>`);
+            newRow.append(`<td><input type="number" class="form-control" id="${formData.iNetAmount}_${currentID}" value="${formData.iNetAmount}" readonly/></td>`);
+            newRow.append(`<td><input type="number" class="form-control" id="${formData.iTotalAmount}_${currentID}" value="${formData.iTotalAmount}" readonly/></td>`);
+            newRow.append(`<td><button class="btn btn-danger remove" id="removeAllButton">Remove</button></td>`);
 
             // Add the new row to the table body
             $('#tableBody').append(newRow);
         });
 
 
-        // Add a click event listener to the "Remove All" button
-        $('#tableBody').on('click', '#removeAllButton', function () {
+        //function to calculate Net Amount
+        $('#iDiscountedPrice').change(function() {
 
+            // Calculate the net amount
+            const iPrice = $('#ProductPrice').val();
+
+            const iDiscountedPrice = $(this).val();
+            const iNetAmount = iPrice - (iDiscountedPrice / 100) * iPrice;
+
+            // Set the value of the net amount input field
+            $('#ProductNetAmount').val(iNetAmount);
+        });
+
+
+        // Add a click event listener to the "Remove All" button
+        $('#tableBody').on('click', '.remove', function() {
             // Remove the current row from the table
             $(this).closest('tr').remove();
         });
 
 
-        $('#insertData').click(function () {
+        $('#insertData').click(function() {
             // Get the form data
             var formData = {
                 vCustomerName: $('#vCustomerName').val(),
@@ -245,7 +251,7 @@ include('connect.php');
                 method: 'POST',
                 data: formData,
                 dataType: 'json',
-                success: function (data) {
+                success: function(data) {
                     // console.log(data.message);
                     alert(data.message);
                     window.location.replace('index.php');
